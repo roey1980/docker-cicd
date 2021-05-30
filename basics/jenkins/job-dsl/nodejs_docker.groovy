d1368cc877a9bfefdaa63b92eb9ed1e8d475acca
roey1980 <84897337+roey1980@users.noreply.github.com>
2021-05-30 12:23:48 +0300

    Update nodejs_docker.groovy

--- basics/jenkins/job-dsl/nodejs_docker.groovy
+++ basics/jenkins/job-dsl/nodejs_docker.groovy
@@ -2,7 +2,7 @@ job('NodeJS Docker example') {
     scm {
         git('git://github.com/roey1980/docker-cicd.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@ndomain.com')
+            node / gitConfigEmail('jenkins-dsl@domain.com')
         }
     }
     triggers {
@@ -14,7 +14,6 @@ job('NodeJS Docker example') {
     steps {
         dockerBuildAndPublish {
             repositoryName('roeyydocker/nodejsdocker')
-            buildContext('basics/jenkins/job-dsl/nodejs_docker.groovy')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
