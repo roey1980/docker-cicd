35b32b1f48e0809abd0aa44d0e96a1f0c3fd468d
roey1980 <84897337+roey1980@users.noreply.github.com>
2021-05-30 10:55:42 +0300

    Update nodejs.groovy

--- basics/dsl/nodejs.groovy
+++ basics/dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/roey1980/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('jenkins-dsl@domain.com')
         }
     }
     triggers {
