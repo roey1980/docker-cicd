77593422e005cc89536403092d221d0e7a0688c1
roey1980 <84897337+roey1980@users.noreply.github.com>
2021-05-30 12:14:11 +0300

    Update nodejs_docker.groovy

--- basics/jenkins/job-dsl/nodejs_docker.groovy
+++ basics/jenkins/job-dsl/nodejs_docker.groovy
@@ -14,6 +14,7 @@ job('NodeJS Docker example') {
     steps {
         dockerBuildAndPublish {
             repositoryName('roeyydocker/nodejsdocker')
+            buildContext('basics/jenkins/job-dsl/nodejs_docker.groovy')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
