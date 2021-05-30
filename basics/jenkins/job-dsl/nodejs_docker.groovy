a1b15307842b3811910456160ec482a8a63093ad
roey1980 <84897337+roey1980@users.noreply.github.com>
2021-05-30 11:27:51 +0300

    Update nodejs_docker.groovy

--- basics/jenkins/job-dsl/nodejs_docker.groovy
+++ basics/jenkins/job-dsl/nodejs_docker.groovy
@@ -1,19 +1,19 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/roey1980/docker-cicd.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('jenkins-dsl@ndomain.com')
         }
     }
     triggers {
         scm('H/5 * * * *')
     }
-    wrappers {
-        nodejs('nodejs-new') 
-    }
+//     wrappers {
+//         nodejs('nodejs-new') 
+//     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('aaaaaaaaa/docker-nodejs-demo')
+            repositoryName('roeyydocker/nodeapp')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
